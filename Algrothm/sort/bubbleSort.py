6646f8c0aa6fc873a15115b6459bf5c6ca914b16
ZHANGjind6 <495350559@QQ.COM>
2021-03-13 21:48:03 +0800

    update

--- Algrothm/sort/bubbleSort.py
+++ Algrothm/sort/bubbleSort.py
@@ -11,7 +11,6 @@
 '''
 
 
-
 def bubble_sort_v1(arr=[]):
     for i in range(len(arr) - 1):
         for j in range(len(arr) - i - 1):
@@ -53,6 +52,34 @@ def bubble_sort_v3(arr=[]):
             break
 
 
-mylist = [45, 4, 7, 44, 3, 7, 8, 2, 9, 13, 6, 10, 34]
-bubble_sort_v3(mylist)
+def bubble_sort_v4(arr=[]):
+    for i in range(len(arr) // 2):
+        is_sort = True  # 如果没有发生交换，则说明是有序的，否则，需要重新遍历一次
+        #奇数轮，从左到右比较交换
+        for j in range(i, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                tmp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
+                is_sort = False
+
+        if is_sort:
+            break
+        #偶数轮，之前重新标记
+        is_sort = True
+        #偶数轮，从右到左比较交换
+        for j in range(len(arr) - i - 1, i, -1):
+            print(j)
+            if arr[j] < arr[j - 1]:
+                tmp = arr[j]
+                arr[j] = arr[j - 1]
+                arr[j - 1] = tmp
+                is_sort = False
+
+        if is_sort:
+            break
+
+
+mylist = ([45, 4, 7, 44, 3, 7, 8, 2, 9, 13, 6, 10, 34])
+bubble_sort_v4(mylist)
 print(mylist)
