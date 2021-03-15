8da66362dac79892a624f15c3015310440405475
liutao <495350559@qq.com>
2021-03-15 12:43:24 +0800

    update

--- Algrothm/binarySearch/binarySearch.py
+++ Algrothm/binarySearch/binarySearch.py
@@ -10,4 +10,50 @@
 2021/3/15 12:25 下午   Liutaou      1.0         None
 '''
 
+
 # import lib
+
+def binarySearch_v1(arr, target):
+    if len(arr) == 0:
+        return -1
+
+    left, right = 0, len(arr) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            return target
+        elif target < arr[mid]:
+            right == mid - 1
+        else:
+            left == mid + 1
+
+    return -1
+
+
+def binarySearch_v2(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    if len(nums) == 0:
+        return -1
+
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            print("target = "+str(target))
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    # End Condition: left > right
+    return -1
+
+
+# binarySearch_v1([0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 23], 6)
+binarySearch_v2([0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 23], 6)
