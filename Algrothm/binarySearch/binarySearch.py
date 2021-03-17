cb3b3ff4dccd5f5a04072c5601f9c173fa0146a7
liutao <495350559@qq.com>
2021-03-17 23:14:13 +0800

    update

--- Algrothm/binarySearch/binarySearch.py
+++ Algrothm/binarySearch/binarySearch.py
@@ -49,10 +49,33 @@ def binarySearch_v1(nums, target):
 模版-2
 初始条件：left = 0, right = length-1
 终止：left > right
-向左查找：right = mid-1
+向左查找：right = mid
 向右查找：left = mid+1
+
+一种实现二分查找的高级方法。
+查找条件需要访问元素的直接右邻居。
+使用元素的右邻居来确定是否满足条件，并决定是向左还是向右。
+保证查找空间在每一步中至少有 2 个元素。
+需要进行后处理。 当你剩下 1 个元素时，循环 / 递归结束。 需要评估剩余元素是否符合条件。
 """
 
-# def binarySearch_v2(nums, target):
+
+def binarySearch_v2(nums, target):
+    if len(nums) == 0:
+        return -1
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    if left != len(nums) and nums[left] == target:
+        return left
+    return -1
+
 
 binarySearch_v1([0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 23], 6)
+print(binarySearch_v2([0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 23], 6))
