603d7fd3473d0ecf1e17fbdd02c894a2cb8453b4
liutao <495350559@qq.com>
2021-03-17 22:15:46 +0800

    update

---
+++ Algrothm/binarySearch/searchMoveArr.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   searchMoveArr.py    
+@Contact :   liutao0705@live.com
+@License :   (C)Copyright 2021-2022
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2021/3/16 7:32 下午   Lita       1.0         None
+"""
+
+"""
+整数数组 nums 按升序排列，数组中的值 互不相同 。
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的索引，否则返回 -1 。
+"""
+
+
+def search(nums, target):
+    if len(nums) == 0:
+        return -1
+
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[0] <= nums[mid]:
+            if nums[0] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[len(nums) - 1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+
+
+def search_v2(nums, target):
+    if not nums:
+        return -1
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[0] <= nums[mid]:
+            if nums[0] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if nums[mid] < target <= nums[len(nums) - 1]:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return -1
+
+
+myNums = ([4, 5, 6, 7, 0, 1, 2])
+# search(myNums,8)
+print(search(myNums, 0))
+print(search_v2(myNums, 0))
