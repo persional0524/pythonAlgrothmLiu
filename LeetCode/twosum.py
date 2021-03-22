abff8e038683881f2a98ba8b224f55586c0a76f2
liutao <495350559@qq.com>
2021-03-22 23:58:24 +0800

    update

--- LeetCode/twosum.py
+++ LeetCode/twosum.py
@@ -9,3 +9,15 @@
 ------------      -------    --------    -----------
 2021/3/22 9:42 下午   Lita       1.0         None
 """
+
+
+def twosum_v1(arr, target):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if target == arr[i] + arr[j]:
+                return [i, j]
+    return []
+
+
+my_list = ([2, 15, 11, 7])
+print(twosum_v1(my_list, 9))
