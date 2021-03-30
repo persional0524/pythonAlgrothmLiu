becffff60ff922a31837e29fd59794d0adef9b33
liutao <495350559@qq.com>
2021-03-30 16:01:18 +0800

    update

--- Algrothm/sort/bucketSort.py
+++ Algrothm/sort/bucketSort.py
@@ -12,6 +12,7 @@
 
 
 def bucket_sort_v1(arr=[]):
+    # 1.得到数列的最大值最小值，计算差值d
     max_v = arr[0]
     min_v = arr[0]
     for i in range(1, len(arr)):
@@ -20,18 +21,23 @@ def bucket_sort_v1(arr=[]):
         if arr[i] < min_v:
             min_v = arr[i]
     d = max_v - min_v
+    # 2.初始化桶
     bucket_num = len(arr)
     bucket_list = []
     for i in range(len(arr)):
         bucket_list.append([])
+    # 3.遍历原始数组，将每个越元素放入桶中
     for i in range(len(arr)):
         num = int((arr[i] - min_v) * (bucket_num - 1) / d)
         print("bucket_list=" + str(bucket_list))
         print("num=" + str(num))
         bucket = bucket_list[num]
         bucket.append(arr[i])
+    # 4.对每个桶内部进行排序
     for i in range(len(bucket_list)):
+        # sort 方法采用了时间复杂度O(nlogn)的排序算法
         bucket_list[i].sort()
+    # 5.输出全部元素
     sort_arr = []
     for sub_list in bucket_list:
         for element in sub_list:
---
+++ Module/Hightcharts/HightchartsP1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   HightchartsP1.py    
+@Contact :   liutao0705@live.com
+@License :   (C)Copyright 2021-2022
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2021/3/30 10:21 上午   Lita       1.0         None
+"""
+
+
+from highcharts import Highchart
+chart = Highchart()
+
+
