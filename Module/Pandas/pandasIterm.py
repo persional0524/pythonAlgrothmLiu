eddf849917f8a1899ba5efab2b973cff403ce7d0
liutao <495350559@qq.com>
2021-04-08 17:02:40 +0800

    update

--- Module/Pandas/pandasIterm.py
+++ Module/Pandas/pandasIterm.py
@@ -11,7 +11,6 @@
 """
 
 import pandas as pd
-from numpy import integer
 
 """
 简单对上面三种方法进行说明：
@@ -21,32 +20,27 @@ itertuples(): 按行遍历，将DataFrame的每一行迭代为元祖，可以通
 iteritems():按列遍历，将DataFrame的每一列迭代为(列名, Series)对，可以通过row[index]对元素进行访问。
 """
 
-inp = [{'c1':10, 'c2':100}, {'c1':11, 'c2':110}, {'c1':12, 'c2':123}]
+inp = [{'c1': 10, 'c2': 100}, {'c1': 11, 'c2': 110}, {'c1': 12, 'c2': 123}]
 df = pd.DataFrame(inp)
 
 print(df)
 
 print("[1] 按行遍历iterrows():")
 for index, row in df.iterrows():
-    print(index) # 输出每行的索引值
+    print(index)  # 输出每行的索引值
 
 print("[1.1] 按行遍历iterrows():")
 # 对于每一行，通过列名name访问对应的元素
-for index,row in df.iterrows():
-    print(row['c1'], row['c2']) # 输出每一行
+for index, row in df.iterrows():
+    print(row['c1'], row['c2'])  # 输出每一行
 
 print("[2] 按行遍历itertuples():")
 for row in df.itertuples():
-    print(getattr(row, 'c1'), getattr(row, 'c2')) # 输出每一行
+    print(getattr(row, 'c1'), getattr(row, 'c2'))  # 输出每一行
 
 print("[3] 按列遍历iteritems():")
 for index, row in df.iteritems():
-
-    print(index) # 输出列名
+    print(index)  # 输出列名
 print("[3.1] 按列遍历iteritems():")
-for index,row in df.iteritems():
-    print(row[0], row[1], row[2]) # 输出各列
-
-
-
-
+for index, row in df.iteritems():
+    print(row[0], row[1], row[2])  # 输出各列
