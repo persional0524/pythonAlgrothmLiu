2d7aaf39e8af8bff68ff0f97f71e872b1a0b95d3
liutao <495350559@qq.com>
2021-04-14 23:55:02 +0800

    update

--- Module/multiprocessing/threadCopy.py
+++ Module/multiprocessing/threadCopy.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 """
-@File    :   multipCopy.py    
+@File    :   threadCopy.py    
 @Contact :   liutao0705@live.com
 @License :   (C)Copyright 2021-2022
 
 @Modify Time      @Author    @Version    @Desciption
 ------------      -------    --------    -----------
-2021/4/14 6:13 下午   Lita       1.0         None
+2021/4/14 11:23 下午   Lita       1.0         None
 """
 
-
-# 多进程，实现高并发的效果
+# 多线程，实现高并发的效果
 import os
-import multiprocessing as mp
+import threading as td
 
 
 def copy_file(file_name, source_dir, dest_dir):
@@ -34,7 +33,7 @@ def copy_file(file_name, source_dir, dest_dir):
 
 if __name__ == '__main__':
     source_dir = "/Users/liutao/PycharmProjects/pythonAlgrothmLiu/Algrothm/sort"
-    dest_dir = "/Users/liutao/Desktop/test1"
+    dest_dir = "/Users/liutao/Desktop/test"
     try:
         os.mkdir(dest_dir)
     except:
@@ -44,5 +43,5 @@ if __name__ == '__main__':
     for file_name in file_list:
         # copy_file(file_name, source_dir, dest_dir)
         # 创建子线程，震开销更小，功能一样可以实现
-        sub_thread = mp.Process(target=copy_file, args=(file_name, source_dir, dest_dir))
+        sub_thread = td.Thread(target=copy_file, args=(file_name, source_dir, dest_dir))
         sub_thread.start()
